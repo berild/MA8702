54475ac279b72895db0cd90a04ccd78ab7b42c5f
berild <moutzebe@gmail.com>
2021-02-09 16:44:07 +0100

    add diagnostics oppgave2

--- project-1/oppgave2.R
+++ project-1/oppgave2.R
@@ -1,4 +1,7 @@
 library(rstan) # observe startup messages
+library(dplyr)
+library(purrr)
+library(coda)
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 pump_data <- list(N = 10,
@@ -17,7 +20,28 @@ fit <- stan(
 )
 
 res <- as.data.frame(fit)
+res = res[,-ncol(res)]
 
-library(coda)
+res = cbind(steps = seq(nrow(res)),res)
+
 
 effectiveSize(res)
+res
+df = reshape2::melt(res ,  id.vars = 'steps', variable.name = 'vars')
+ggplot(df, aes(steps,value)) + geom_line() + facet_grid(vars ~ .,scales = "free")
+ggplot(df, aes(x = value, y=..density..)) + geom_histogram() + facet_wrap(vars ~ .,scales = "free")
+
+bacdf <- map_df(res, function(ts) as.vector(acf(ts, plot = FALSE)$acf))
+
+# The lags are all the same just 0 through the number of rows minus 1
+
+bacdf$lag <- 0:(nrow(bacdf) - 1)
+
+# reorder things and eliminate `Date` and unclean AAPL which is actually identical to AAPL
+
+bacdf <- bacdf %>% select(lag, everything(), -steps)
+
+bacdf = reshape2::melt(bacdf ,  id.vars = 'lag', variable.name = 'vars')
+
+ggplot(bacdf, aes(lag,value)) + geom_segment(mapping = aes(xend = lag, yend = 0)) + geom_point() +facet_grid(vars ~ .,scales = "free") + theme_minimal()
+
