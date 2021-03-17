2dd5d1d457c2da7e211036cb61230a664bc32b17
hgryvill <hakon.gryvill@ntnu.no>
2021-03-17 17:02:41 +0100

    Update Project 2.R

--- project-2/Project 2.R
+++ project-2/Project 2.R
@@ -1,14 +1,14 @@
 
 library(mvtnorm)
 library(ggplot2)
-library(purrr)
+library(gtools)
 
 # a)
 
-mu = 0
-sigma = 1
-n = 100
-x = mvrnorm(n=n, mu=mu, Sigma = sigma) # vector from multivariate normal
+n = 100 # dimension of x
+mu = rep(0,n) # mean E(x_i)
+sigma = 1 # variance Var(x_i)
+
 
 
 corr_func <- function(i, j){ # returns correlation between x_i and x_j
@@ -16,50 +16,85 @@ corr_func <- function(i, j){ # returns correlation between x_i and x_j
 }
 
 
-Sigma = matrix(data=NA, nrow=n, ncol=n)
+Sigma = matrix(data=NA, nrow=n, ncol=n) # covariance matrix of x
 for (i in seq(1,n)){
   for (j in seq(1,n)){
     Sigma[i,j] = corr_func(i,j)
   }
 }
 
-#ggplot(data.frame(x = seq(1,n),y=seq(1,n),z=Sigma)) + geom_contour_filled(aes(x=x,y=y,z=z))
+
+x = mvrnorm(n=1, mu=mu, Sigma = Sigma) # vector from multivariate normal distribution
+plot(x)
+
 image(Sigma)
 
 # b)
 
-L = chol(Sigma)
+L = chol(Sigma) # Cholesky decomposition of Sigma. Sigma = L L^T 
 image(L)
 
 
 # c)
 
-z = mvrnorm(n=n, mu=rep(0,n), Sigma=diag(n))
-x_chol = L%*%z
+z = mvrnorm(n=1, mu=rep(0,n), Sigma=diag(n)) # vector of multivariate standard normal
+x_chol = L%*%z # x = Lz
 plot(x_chol)
+
+
 
 # d)
 
-Q = solve(Sigma)
+Q = solve(Sigma) # Q = Sigma^{-1}
 image(Q)
-L_Q = chol(Q)
+L_Q = chol(Q) # Sigma^{-1} = L_Q L_Q^T
 image(L_Q)
+
+
 
 # e)
 
-z = mvrnorm(n=n, mu=rep(0,n), Sigma=diag(n))
+z = mvrnorm(n=1, mu=rep(0,n), Sigma=diag(n))
 x_chol_Q = solve(L_Q, z)
 plot(x_chol_Q)
 
+
+
 # f)
 
+perm_pos = permute(seq(1,n)) # permuted positions 
+
+Sigma_perm = matrix(data=NA, nrow=n, ncol=n)
+for (i in seq(1,n)){
+  for (j in seq(1,n)){
+    Sigma_perm[i,j] = corr_func(perm_pos[i], perm_pos[j])
+  }
+}
+
+image(Sigma_perm)
+x_perm = mvrnorm(n=1, mu=mu, Sigma = Sigma) # vector from multivariate normal distribution
+plot(seq(1,n),x_perm)
 
 
 
 
+L_perm = chol(Sigma_perm) # Cholesky decomposition of Sigma. Sigma = L L^T 
+image(L_perm)
 
 
+z = mvrnorm(n=1, mu=rep(0,n), Sigma=diag(n)) # vector of multivariate standard normal
+x_chol_perm = L_perm%*%z # x = Lz
+plot(x_chol_perm)
 
+Q_perm = solve(Sigma_perm) # Q = Sigma^{-1}
+image(Q_perm)
+L_Q_perm = chol(Q_perm) # Sigma^{-1} = L_Q L_Q^T
+image(L_Q_perm)
+
+
+z = mvrnorm(n=1, mu=rep(0,n), Sigma=diag(n))
+x_chol_Q_perm = solve(L_Q_perm, z)
+plot(x_chol_Q_perm)
 
 
 
